24813764cae4a5d3e7b22c6fad1dbfffee65cb93
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-09-14 21:40:19 +0900

    audio file 추가, 트랙 파트 구현

--- src/App.js
+++ src/App.js
@@ -1,57 +1,31 @@
 import React from "react";
-import StebButton from "./components/stebButton.js";
+import TrackStep from "./components/TrackStep";
 import styled from "styled-components";
 import "./App.css";
+// import * as Tone from "tone";
 
-const DrumMachine = styled.div`
-  width: 90%;
-  display: grid;
-  grid-template-rows: repeat(4, 1fr);
-  grid-template-columns: repeat(8, 1fr);
-  justify-items: center;
-  margin: 0 auto;
-  margin-top: 4rem;
-  border: 1px solid black;
-  padding: 1rem;
+const Track = styled.div`
+	width: 90%;
+	display: grid;
+	grid-template-rows: repeat(4, 1fr);
+	grid-template-columns: repeat(8, 1fr);
+	justify-items: center;
+	margin: 0 auto;
+	margin-top: 4rem;
+	border: 1px solid black;
+	padding: 1rem;
 `;
 
 function App() {
-  return (
-    <DrumMachine>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-      <StebButton></StebButton>
-    </DrumMachine>
-  );
+	return (
+		<Track>
+			{/* <StebButton note={"b4"}></StebButton>
+			<StebButton note={"c5"}></StebButton>
+			<StebButton note={"b4"}></StebButton>
+			<StebButton note={"c5"}></StebButton> */}
+			<TrackStep></TrackStep>
+		</Track>
+	);
 }
 
 export default App;
---
+++ src/components/TrackStep.js
@@ -0,0 +1,44 @@
+import React, { useRef, useState } from "react";
+import styled from "styled-components";
+import * as Tone from "tone";
+import Kick from "../audio/kick.wav";
+
+const StepBtn = styled.button`
+	width: 5rem;
+	height: 5rem;
+`;
+
+function TrackStep() {
+	const kick = new Tone.Player(Kick).toDestination();
+	// 사운드 연결 import 부분에서 url을 가져와야 편함.
+
+	const kickSeq = new Tone.Sequence(
+		(time, note) => {
+			kickRef.current.start();
+		},
+		[0],
+		"8n"
+	).start();
+	// 루핑 제작 부분 Sequencd, Part, scheduleRepeat 중 하나 골라야 할듯
+
+	const kickRef = useRef(kick);
+
+	const play = () => {
+		kickRef.current.start();
+	};
+	// 누르면 재생
+
+	const testPlay = () => {
+		Tone.Transport.start();
+	};
+	// 루프 재생
+
+	return (
+		<>
+			<StepBtn onClick={play}></StepBtn>
+			<StepBtn onClick={testPlay}></StepBtn>
+		</>
+	);
+}
+
+export default TrackStep;
